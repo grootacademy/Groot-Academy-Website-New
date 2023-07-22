f9dd20bd0712c1368190671a76b830129f767498
satnamkhowal <satnamkhowal@gmail.com>
2023-07-22 11:26:23 +0530

    owejoi

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -20,16 +20,32 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+
+// local data base
+//define( 'DB_NAME', 'groot_academy' );
+
+/** Database username */
+//define( 'DB_USER', 'root' );
+
+/** Database password */
+//define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+//define( 'DB_HOST', 'localhost' );/
+
+
+
+
 define( 'DB_NAME', 'groot_academy' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u232016825_satnam' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'HareRam@987' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'u232016825_groot_academy' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
