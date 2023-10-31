1037ddf1b66e2cf5259816c682f2a3d55b7d4515
satnamkhowal <satnamkhowal@gmail.com>
2023-10-31 11:10:13 +0530

    hello

--- includes/connection.php
+++ includes/connection.php
@@ -10,17 +10,18 @@
 
 // open
 
-// $servername = "31.220.110.201:3306";
+$servername = "31.220.110.201:3306";
 
-// $username = "u232016825_swiggywala";
-// $password = "HareRam@987";
-// $my_db = 'u232016825_leads';
-// //Create connection
-// $conn = new mysqli($servername, $username, $password, $my_db);
+$username = "u232016825_swiggywala";
+$password = "HareRam@987";
+$my_db = 'u232016825_leads';
+//Create connection
+$conn = new mysqli($servername, $username, $password, $my_db);
 
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
-// // 
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+// 
+?>
