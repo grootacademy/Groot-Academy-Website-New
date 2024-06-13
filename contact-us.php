71a19a928d23d121ca2c01c29b33a1f5b4546de2
satnamkhowal <satnamkhowal@gmail.com>
2024-06-13 18:08:24 +0530

    full stack web development by groot academy phone number updaed

--- contact-us.php
+++ contact-us.php
@@ -255,9 +255,9 @@ stack Web development with python Django , Full stack Web development with PHP, 
     <div class="  bg-gradient-primary  branch-box4">
       <div class=" bg-gradient-primary branch-box1 p-3">
         <center>
-          <b style="font-size: small; box-shadow:2px 2px 10px 5px white; border-radius:10px" class="text-primary p-2 bg-light">Riddhi Shiddi Branch with infiquee classes.</b>
-          <h6> <i class="fa fa-mobile" aria-hidden="true"></i> Phone:<a class="nav-link nav-link-icon me-2" href="tel:+91-8233266276"> +91-8233266276</a></h6>
-          <h6> <i class="fa fa-envelope" aria-hidden="true"></i> Email: infiquee@grootacademy.com</h6><br />
+          <b style="font-size: small; box-shadow:2px 2px 10px 5px white; border-radius:10px" class="text-primary p-2 bg-light">Full stack web development by Groot Academy</b>
+          <h6> <i class="fa fa-mobile" aria-hidden="true"></i> Phone:<a class="nav-link nav-link-icon me-2" href="tel:+91-8233266276"> +91-8094170324</a></h6>
+          <h6> <i class="fa fa-envelope" aria-hidden="true"></i> Email: fullstack@grootacademy.com</h6><br />
           <h6> <i class="fa fa-map-marker " aria-hidden="true"></i> Plot no 26, JAIPUR ELECTRONIC MARKET, Mansarovar Link Rd, in front of manglam, Mohan Nagar, Triveni Nagar, Arjun Nagar, Jaipur, Rajasthan 302019 </h6>
         </center>
       </div>
@@ -275,11 +275,11 @@ stack Web development with python Django , Full stack Web development with PHP, 
   </div> -->
   <!-- </div>/ -->
   <div class="row">
-    <h3>kumabh Marg Branch</h3>
+    <h3>Full stack web development by Groot Academy</h3>
     <div class="col-md-12" style="box-shadow: 20px 0px 5px 0px black;">
       <div class="map-canvas">
         <!-- <iframe src="https://maps.google.com/maps?q=rstheme&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"></iframe> -->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14246.000284489228!2d75.8168402!3d26.7922014!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396dc90069e786cf%3A0xd43882d862c36984!2sGroot%20Academy!5e0!3m2!1sen!2sin!4v1712121425331!5m2!1sen!2sin"  width="100%" height="400"   style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3559.005666339855!2d75.7732728736698!3d26.871561061861108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db5e458a8e5c1%3A0xb39a0134f5dbcb!2sFull%20stack%20web%20development%20by%20Groot%20Academy!5e0!3m2!1sen!2sin!4v1718282217105!5m2!1sen!2sin"  width="100%" height="400"   style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
 
       <div class="row">
         <div class="col-md-12" style="box-shadow: 20px 0px 5px 0px black;">
